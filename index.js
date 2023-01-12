5b30e80773cb447492250fbd2afee76316055478
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-12 00:52:55 +0000

    Create a user object

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ let PORT = 3000;
 
 const app = express();
 
+
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+};
+  
+  let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+};
+
+
+
 app.get('/users', (req, res) => {
     return res.send('GET HTTP method on user resource');
 });
