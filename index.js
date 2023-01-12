4f5a1fd2ef825afc4c7408d2392cf113d12d44ea
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-12 00:50:54 +0000

    Implement the CRUD actions on the  user endpoints

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ let PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method');
+app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+    return res.send('PUT method on user resource');
+});
+  
+app.put('/users/:userId', (req, res) => {
+    return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+  
+app.delete('/:userId', (req, res) => {
+    return res.send(`DELETE HTTP methor on user/${req.params.userId} resource`);
 });
 
 app.listen(PORT, ()=>{
